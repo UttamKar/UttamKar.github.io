2b4b734c6cc1bf649ccad36e15b0eb78000599c3
UttamKar <uttamece28@gmail.com>
2018-03-03 12:45:51 +0600

    portfolio

--- js/main.js
+++ js/main.js
@@ -1,9 +1,58 @@
-jQuery(document).ready(function($){
-	$('#fourthSection > .row > div').equalHeights();
+$(document).ready(function(){
+	/*preloader*/
+		var	overlay = document.getElementById('preloader');
+			window.addEventListener('load', function() {
+				overlay.style.display ='none';
+		});
+
+	/*Go To Top*/
+		$(window).scroll(function(){
+			if($(this).scrollTop()>300){
+				$('.gototop').fadeIn();
+				}
+			else{
+				$('.gototop').fadeOut();
+			}
+		});
+		$('.gototop').click(function(){
+			$('html, body').animate({scrollTop:0}, 500);
+		});
+
+	/*Navigation Bar*/
+		var scroll = 0;
+		$(window).on('scroll', function(){
+			/*Hide header on scroll down show on scroll up*/
+			if(scroll < $(window).scrollTop()){
+				$("#navBar").addClass('hide-menu');
+			}else{
+				$("#navBar").removeClass('hide-menu');
+			}
+			scroll = $(window).scrollTop();
+				
+			/*To change bg color of navbar based on scroll*/
+			var colorValueIncrement = Math.round(scroll/20) ;
+			var bodyColor = $('.navbar-default').css('background');
+			var lastComma = bodyColor.lastIndexOf(',');
+			var newColor = bodyColor.slice(0, lastComma + 1) + Math.round(colorValueIncrement) + ')';
+			$('.navbar-default').css({'background': newColor});
+
+		});
+	/*Navbat button toggle*/
+		$('#navBar button').click(function(){
+			$(this).toggleClass('active');
+		});
+
+
+
+	/*Wow Js activation*/
+		new WOW().init();
+
+	/*pen delayed fadeIn*/
+		$( ".pen" ).slideUp( 300 ).delay( 800 ).fadeIn( 300 );
+
 	
 
-	/*Navbat button toggle*/
-	$('#navBar button').click(function(){
-		$(this).toggleClass('active');
-	});
-})
+
+});
+
+
